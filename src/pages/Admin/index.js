4bdfe08ae988a9f4b51880c245df1468f3c4c194
Fabio Kulka <fabiokulka.67@gmail.com>
2024-04-17 15:12:47 -0300

    Listando todas as tarefas

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,45 +1,112 @@
 import './admin.css';
-import { useState } from 'react';
-import { auth} from '../../firebaseConnection';
+import { useState, useEffect } from 'react';
+import { auth, db } from '../../firebaseConnection';
 import { signOut } from 'firebase/auth';
+import {
+    addDoc
+    , collection
+    , onSnapshot
+    , query
+    , orderBy
+    , where
+    ,doc
+    ,deleteDoc
+} from 'firebase/firestore';
 
-export default function Admin(){
+export default function Admin() {
 
     const [tarefaInput, setTarefaInput] = useState('');
+    const [user, setUser] = useState({});
+    const [tarefas, setTarefas] = useState([]);
 
-    function handleRegister(e){
+    useEffect(() => {
+        async function loadTarefas() {
+
+            const userDetail = localStorage.getItem('@detailUser');
+            setUser(JSON.parse(userDetail));
+
+            if (userDetail) {
+                const data = JSON.parse(userDetail);
+                const tarefaRef = collection(db, "tarefas")
+                const q = query(tarefaRef, orderBy("created", "desc"), where("userUid", "==", data?.uid))
+                const unsub = onSnapshot(q, (snapshot) => {
+                    let lista = [];
+                    snapshot.forEach((doc) => {
+                        lista.push({
+                            id: doc.id,
+                            tarefa: doc.data().tarefa,
+                            userUid: doc.data().userUid
+                        })
+                    })
+                    setTarefas(lista);
+                })
+            }
+        }
+
+        loadTarefas();
+    }, [])
+
+    async function handleRegister(e) {
         e.preventDefault();
-        alert('Clicou')
+        if (tarefaInput === '') {
+            alert("Digite a sua tarefa")
+            return;
+        }
+        await addDoc(collection(db, "tarefas"), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUid: user?.uid
+        })
+            .then(() => {
+                console.log("Tarefa registrada com sucesso");
+                setTarefaInput('');
+            })
+            .catch((error) => {
+                console.log("Erro ao registrar" + error);
+            })
+
     }
 
-    async function handleLogOut(){
+    async function handleLogOut() {
         await signOut(auth);
     }
 
-    return(
+    async function deleteTarefa(id){
+        const docRef = doc(db, "tarefas", id)
+        await deleteDoc(docRef);
+    }
+
+    function editTarefa(item){
+        setTarefaInput(item.tarefa)
+    }
+
+
+    return (
         <div className='admin-container'>
             <h1>Minhas tarefas</h1>
 
-            <form  className='form' onSubmit={handleRegister}>
+            <form className='form' onSubmit={handleRegister}>
                 <textarea
-                placeholder='Digite a sua tarefa'
-                value={tarefaInput} 
-                onChange={(e) => setTarefaInput(e.target.value)}
+                    placeholder='Digite a sua tarefa'
+                    value={tarefaInput}
+                    onChange={(e) => setTarefaInput(e.target.value)}
                 />
-                <button  className='btn-register' type='submit'>Registrar tarefa</button>
+                <button className='btn-register' type='submit'>Registrar tarefa</button>
             </form>
 
-            <article className='list'>
-                <p>Estudar javascript e reactjs</p>
+            {tarefas.map((item) => (
+                <article  key={item.id} className='list'>
+                    <p>{item.tarefa}</p>
 
-                <div>
-                    <button>Editar</button>
-                    <button className='btn-delete'>Concluir</button>
-                </div>
-            </article>
+                    <div>
+                        <button onClick={() => editTarefa(item)}>Editar</button>
+                        <button className='btn-delete' onClick={() => deleteTarefa(item.id)}>Concluir</button>
+                    </div>
+                </article>
+            ))}
 
             <button className='btn-logout' onClick={handleLogOut}>Sair</button>
-            
+
         </div>
     );
 }
